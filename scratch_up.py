063178e8da8fbf3829154b6f36688c2a798d55aa
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-06-07 12:24:10 +0300

    + API_KEY

--- scratch_up.py
+++ scratch_up.py
@@ -1,10 +1,13 @@
 import psycopg2
+import os
 
-conn = psycopg2.connect(host="localhost", database="test", user="postgres", password="310715")
+POSTGRES_API_KEY = os.getenv('POSTGRES_API_KEY')
+
+conn = psycopg2.connect(host="localhost", database="test", user="postgres", password=POSTGRES_API_KEY)
 try:
     with conn:
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO user_account VALUES (%s, %s)", (8, "Yamamma"))
+            cur.execute("INSERT INTO user_account VALUES (%s, %s)", (9, "Настя"))
 
             cur.execute("SELECT * FROM user_account")
             rows = cur.fetchall()
